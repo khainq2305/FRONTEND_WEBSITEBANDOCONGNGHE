55ed70dc963a791977241b16705ea32396da70a9
phuoclhpc08607 <phuoclhpc08607@gmail.com>
2025-05-17 22:37:11 +0700

    [Admin] - Fix routes user

--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -9,7 +9,8 @@ const DashboardDefault = Loadable(lazy(() => import('pages/Admin/Dashboard')));
 const BasicTableDemo = Loadable(lazy(() => import('pages/Admin/BasicTableDemo'))); // <- THÊM DÒNG NÀY
 const OrderList = Loadable(lazy(() => import('pages/Admin/OrderList')));
 const OrderDetail = Loadable(lazy(() => import('pages/Admin/OrderDetail')));
-
+const UserList = Loadable(lazy(() => import('pages/Admin/User/UserList')));
+const UserAdd = Loadable(lazy(() => import('pages/Admin/User/UserAdd')));
 const AdminRoutes = {
   path: '/admin',
   element: <DashboardLayout />,
@@ -33,7 +34,12 @@ const AdminRoutes = {
     {
       path: 'orders/:id',
       element: <OrderDetail />
-    }
+    },
+    {
+      path: 'users',
+      element: <UserList />
+    },
+    { path: 'users/create', element: <UserAdd /> }
   ]
 };
 
