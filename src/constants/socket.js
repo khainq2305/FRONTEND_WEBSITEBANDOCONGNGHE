08caa6e7a137f4dd1e81c557a467e873ccb6a5cf
nguyenquockhai <khainqpc08388@gmail.com>
2025-08-10 00:28:30 +0700

    Fix socket.io client config, add websocket transport, update server init

--- src/constants/socket.js
+++ src/constants/socket.js
@@ -3,7 +3,11 @@ import { io } from 'socket.io-client';
 import { API_BASE_URL } from './environment';
 
 const socket = io(API_BASE_URL, {
-  withCredentials: true
+  transports: ['websocket', 'polling'], // ưu tiên websocket, fallback polling
+  withCredentials: true,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  reconnectionDelay: 2000,
 });
 
 export default socket;
