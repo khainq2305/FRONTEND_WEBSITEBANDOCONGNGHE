56e1dadbf1cbba67faed02f9cd898f593fc5ebd4
Hoangtamdhpc08491 <tamdhpc08491@gmail.com>
2025-08-27 01:05:11 +0700

    Revert "update"
    This reverts commit a44cc83ad208e61bac8fd3252c2813c8eff47d1a.

--- src/layout/Admin/Footer.jsx
+++ src/layout/Admin/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <Stack direction="row" sx={{ alignItems: 'center', justifyContent: 'center', p: '24px 16px 0px', mt: 'auto' }}>
       <Typography variant="caption">
-        &copy; Nguyễn Quốc Khải, Đoàn Thanh Thạnh, Lê Hữu Phước, Trần Thanh Hải, Nguyễn Chí Khanh, Nguyễn Lương Minh Nhựt, Đỗ Hoàng Tâm, Cô Trần Hoàng Lệ Chi
+        &copy; Nguyễn Quốc Khải, Đoàn Thanh Thạnh, Lê Hữu Phước, Trần Thanh Hải, Nguyễn Chí Khanh, Nguyễn Lương Minh Nhựt, Cô Trần Hoàng Lệ Chi
       
       </Typography>
      
